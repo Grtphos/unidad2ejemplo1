58b35d3e4f61f00100aac0ab933b1295778a3e01
Grtphos <86816331+Grtphos@users.noreply.github.com>
2021-10-25 16:48:11 -0600

    Add files via upload

--- holamundo.js
+++ holamundo.js
@@ -1,8 +1,14 @@
-console.log('Hola mundo desde Node.js')
-console.log(2*5)
-console.log(window)
-console.log(globalThis)
+'use strict' //modo estricto para programar con buenas practicas
 
+//imprime en pantalla
+console.log('Hola mundo desde Node.js')
+//hacemos una operacion basica
+console.log(2*5)
+//al utilizar una instruccion de js frontend marca error en el node.js
+//console.log(window)
+//imprime en consola todo lo relacionado con el modulo GLOBAL
+console.log(globalThis)
+//realizamos una funcion para imprimir cada segundo 'Hola Mundo'
 setInterval(function(){
     console.log('Hola Mundo')
 },1000)
